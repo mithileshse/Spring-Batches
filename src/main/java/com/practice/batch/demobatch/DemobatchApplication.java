a18c61541e0ce85f8d17e8ee6dfbad545f75368c
Mithilesh Singh <mithileshsingh@Mithileshs-MacBook-Air.local>
2018-10-18 23:51:37 +0530

    Committing the Basic Batch Project

---
+++ src/main/java/com/practice/batch/demobatch/DemobatchApplication.java
@@ -0,0 +1,12 @@
+package com.practice.batch.demobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemobatchApplication.class, args);
+    }
+}
