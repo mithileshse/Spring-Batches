394e26d8871e3681a4d4ffaa1704f23493256f62
mithileshsingh <mithileshse@gmail.com>
2018-10-21 13:44:33 +0530

    Committing the steps with transition

--- src/main/java/com/practice/batch/demobatch/config/JobConfiguration.java
+++ src/main/java/com/practice/batch/demobatch/config/JobConfiguration.java
@@ -23,22 +23,48 @@ public class JobConfiguration {
     @Autowired
     private StepBuilderFactory stepBuilderFactory;
 
+    private static RepeatStatus execute(StepContribution c, ChunkContext r) {
+        System.out.println("Step 2 Executed");
+        return RepeatStatus.FINISHED;
+    }
+
+    private static RepeatStatus execute2(StepContribution c, ChunkContext r) {
+        System.out.println("Executed Step 3");
+        return RepeatStatus.FINISHED;
+    }
+
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Hello World!");
+                        System.out.println("Executed Step1");
                         return RepeatStatus.FINISHED;
                     }
                 }).build();
     }
 
+    public Step step2(){
+        return stepBuilderFactory.get("step2")
+                .tasklet(JobConfiguration::execute)
+                .build();
+    }
+
+
+    public Step step3(){
+        return stepBuilderFactory.get("step3")
+                .tasklet(
+                        JobConfiguration::execute2
+                )
+                .build();
+    }
     @Bean
     public Job helloWorldJob() {
-        return jobBuilderFactory.get("helloWorldJob1")
+        return jobBuilderFactory.get("transition")
                 .start(step1())
+                .next(step2())
+                .next(step3())
                 .build();
     }
 
